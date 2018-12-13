6de7ace64e919e1597a244437ba1a7e4030b5f39
sugjian <sgj0322@126.com>
2018-12-13 15:28:42 +0800

    add code

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  document.getElementById('main-content').innerHTML = '欢迎来到秋硕教育这个大家庭！'
 }
